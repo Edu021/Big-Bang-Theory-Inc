3722fd61f09ac9852553c5ce3a972cd85164bd21
edu021 <edu.netgaming@gmail.com>
2024-04-01 13:39:24 -0300

    Adição da logica para popular a tabela

--- resposta_1.php
+++ resposta_1.php
@@ -1,6 +1,18 @@
 <!-- 1. Utilizando PHP, escreva um algoritmo que calcule a média dos pedidos por dia e os exiba em um formato de tabela no HTML.  
 Caso a média seja inferior a 3.000, a linha deve ficar vermelha, se for maior, deve ficar verde. E se for igual, deve ser cinza. -->
 
+<?php
+    // Puxando pedidos do banco e colocando tudo em $pedidos
+    include("./conexao.php");
+    
+    $resultado = mysqli_query($conexao, 'select o.order_id, u.user_name, o.order_total, o.order_date from orders o, user u where o.order_user_id = u.user_id;');
+    while($linha = $resultado->fetch_object()) {
+        foreach($linha as $chave => $coluna) {
+            $coluna_array[$chave] = utf8_encode($coluna);
+        }
+        $pedidos[] =  $coluna_array;
+    }
+?>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
@@ -27,6 +39,41 @@ Caso a média seja inferior a 3.000, a linha deve ficar vermelha, se for maior, 
             <th>TOTAL</th>
             <th>DATA</th>
         </tr>
+
+        <?php
+            // Criando linha da tabela com os valores de $pedidos
+            // Cor do fundo muda conforme valor de pedido[order_total]  
+            foreach($pedidos as $pedido) {
+                if($pedido['order_total'] == 3000) {
+                    $linha_tabela = 
+                    "<tr style='background-color: grey;'>
+                    <td>$pedido[order_id]</td>
+                    <td>$pedido[user_name]</td>
+                    <td>$pedido[order_total]</td>
+                    <td>$pedido[order_date]</td>
+                    </tr>";
+                    echo $linha_tabela;
+                } elseif ($pedido['order_total'] > 3000) {
+                    $linha_tabela = 
+                    "<tr style='background-color: green;'>
+                    <td>$pedido[order_id]</td>
+                    <td>$pedido[user_name]</td>
+                    <td>$pedido[order_total]</td>
+                    <td>$pedido[order_date]</td>
+                    </tr>";
+                    echo $linha_tabela;
+                } elseif ($pedido['order_total'] < 3000) {
+                    $linha_tabela = 
+                    "<tr style='background-color: red;'>
+                    <td>$pedido[order_id]</td>
+                    <td>$pedido[user_name]</td>
+                    <td>$pedido[order_total]</td>
+                    <td>$pedido[order_date]</td>
+                    </tr>";
+                    echo $linha_tabela;
+                }
+            }
+        ?>
     </table>
 </body>
 </html>
